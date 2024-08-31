b151453150585113c8e0904bb5d4482dea2efa56
Figeral <mouliofitzgerard@gmail.com>
2024-09-01 00:19:54 +0100

    structuration implementation de l'architecture et separation des composant

---
+++ lib/utils/app_string.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static const appName = "Trash Berry";
+  static const boarding1 = "";
+  static const boarding2 = "";
+  static const boarding3 = "";
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+  static final green = Color(0x306844);
+  static final grey = Color(0x455b55);
+  static final dark = Color(0x182c25);
+  static final deepGreen = Color(0x2c4c3b);
+}
